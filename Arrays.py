ef96742e9eb8ae1e1069ad6aed13eee08ace1ac1
Santosh Belal <107342749+Santosh-Belal@users.noreply.github.com>
2022-11-03 18:46:07 +0545

    Create Arrays.py

---
+++ Arrays.py
@@ -0,0 +1,27 @@
+cars=["Volv","BMW","Lambo"]
+
+#x=cars[0]
+
+cars[0]="Ford"
+
+cars.append("honda")
+
+cars.append("Lambo")
+
+cars.append("Ferrari")
+
+cars.append("Mersedes")
+
+cars.append("Rolls Royalce")
+
+cars.pop(1)
+
+cars.remove("honda")
+
+print(cars)
+
+print(len(cars))
+
+print(cars[0])
+
+print(cars[4])
